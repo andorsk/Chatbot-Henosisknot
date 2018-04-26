b1eb6332a6344829134bf2033e9ed4959abe688e
andorsk <andor.developer@gmail.com>
2018-04-26 10:00:34 +0800

    rule now wraps sequence match rule better from stanfords nlp library

--- src/test/java/com/rulestest/TestReadJSON.java
+++
@@ -1,4 +0,0 @@
-package com.rulestest;
-
-public class TestReadJSON {
-}
---
+++ src/test/java/com/rulestest/TestReadRulesJSON.java
@@ -0,0 +1,60 @@
+package com.rulestest;
+
+import com.proto.gen.RuleOuterClass;
+import com.rules.RulesHelpers;
+import junit.framework.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.UUID;
+
+public class TestReadJSON {
+
+    private String rulesPath;
+
+    @BeforeTest
+    private void setUp(){
+        ClassLoader classLoader = getClass().getClassLoader();
+        rulesPath = "/users/andor/workspace/Chatbot-Henosisknot/src/test/resources/rulesjson/test/test.txt";
+    }
+
+    public RuleOuterClass.Rules writeRules(String path){
+
+        RuleOuterClass.Rule r1 = RuleOuterClass.Rule.newBuilder()
+                .setGuid(UUID.randomUUID().toString())
+                .setPattern("/Pattern/ /This/")
+                .setPositiveMatch("Pattern This")
+                .setNegativeMatch("This doesn't work")
+                .setType("token")
+                .build();
+        RuleOuterClass.Rule r2 = RuleOuterClass.Rule.newBuilder()
+                .setGuid(UUID.randomUUID().toString())
+                .setPattern("/Pattern/ /This/")
+                .setPositiveMatch("Pattern This")
+                .setNegativeMatch("This doesn't work")
+                .setType("token")
+                .build();
+
+        RuleOuterClass.Rules.Builder rb = RuleOuterClass.Rules.newBuilder();
+        rb.addRules(r1);
+        rb.addRules(r2);
+        RuleOuterClass.Rules rules = rb.build();
+        RulesHelpers.printRulesToFileInJSON(rules, path);
+        return rules;
+    }
+
+    @Test
+    public void ReadRulesTest(){
+        RuleOuterClass.Rules wrrls = writeRules(rulesPath);
+        RuleOuterClass.Rules rules = RulesHelpers.readRulesFromJSON(rulesPath);
+        Assert.assertEquals(wrrls, rules);
+        Assert.assertEquals(rules.getRulesList().size(), 2);
+    }
+
+    @AfterClass
+    private void deleteFile(){
+        new File(rulesPath).deleteOnExit();
+    }
+}
